64fd1607f6c5e21d9c2e4557c193cacf900ba750
Alex Curran <alexcurran.va@gmail.com>
2021-05-12 14:44:26 -0500

    Basic test suite setup for Greeting comp

---
+++ src/components/Greeting.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Greeting() {
+    return (
+        <div>
+            <h2>Hello World</h2>
+            <p>It's good</p>
+        </div>
+    );
+}
+
+export default Greeting;
---
+++ src/components/Greeting.test.js
@@ -0,0 +1,17 @@
+import { render, screen } from "@testing-library/react";
+
+import Greeting from "./Greeting";
+
+describe("Greeting component", () => {
+    test("renders Hello World as text", () => {
+        // Arrange
+        render(<Greeting />);
+    
+        // Act
+    
+        // Assert
+        const helloWorldElement = screen.getByText("Hello World");
+    
+        expect(helloWorldElement).toBeInTheDocument();
+    });
+});
