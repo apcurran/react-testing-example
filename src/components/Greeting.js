9c4eb98c43a770d3635ec963b9c1ea8f75a52be3
Alex Curran <alexcurran.va@gmail.com>
2021-05-12 15:29:39 -0500

    Add a test for paragraph text before button is clicked

--- src/components/Greeting.js
+++ src/components/Greeting.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function Greeting() {
+    const [changedText, setChangedText] = useState(false);
+
+    function changeTextHandler() {
+        setChangedText(true);
+    }
+
     return (
         <div>
             <h2>Hello World</h2>
-            <p>It's good</p>
+            {!changedText ? <p>It's good</p> : null}
+            {changedText ? <p>Changed</p> : null}
+            <button onClick={changeTextHandler}>Change Text</button>
         </div>
     );
 }
--- src/components/Greeting.test.js
+++ src/components/Greeting.test.js
@@ -14,4 +14,28 @@ describe("Greeting component", () => {
     
         expect(helloWorldElement).toBeInTheDocument();
     });
+
+    test("renders 'It's good' if the button was NOT clicked", () => {
+        // Arrange
+        render(<Greeting />);
+    
+        // Act
+    
+        // Assert
+        const pElement = screen.getByText("It's good");
+    
+        expect(pElement).toBeInTheDocument();
+    });
+
+    // test("renders Changed if the button WAS clicked", () => {
+    //     // Arrange
+    //     render(<Greeting />);
+    
+    //     // Act
+    
+    //     // Assert
+    //     const helloWorldElement = screen.getByText("Hello World");
+    
+    //     expect(helloWorldElement).toBeInTheDocument();
+    // });
 });
