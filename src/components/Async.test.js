3a01a9a29365ae6de807c9cd858b0182f7242aa5
Alex Curran <alexcurran.va@gmail.com>
2021-05-13 13:37:36 -0500

    Add mock async func to override Fetch API

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,14 @@ import Async from "./Async";
 
 describe("Async component", () => {
     test("renders posts if request succeeds", async () => {
+        // Override browser's Fetch API with mock func from Jest
+        window.fetch = jest.fn();
+        window.fetch.mockResolvedValueOnce({
+            json: async () => {
+                return [{ id: "p1", title: "First Post" }];
+            }
+        });
+
         render(<Async />);
 
         // findAllByRole returns a promise, which can be awaited in an async function
